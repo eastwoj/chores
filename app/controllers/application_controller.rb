c55395c69a44155917016cd3331dedd0d8cbe519
Jason Eastwood <493822+eastwoj@users.noreply.github.com>
2025-08-13 19:46:22 -0500

    Update rails browser list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Allow browsers supporting our minimum requirements including Chrome 93+ and Safari 13+
+  allow_browser versions: { chrome: 93, safari: 13, firefox: 90, edge: 93 }
 
   protected
 
